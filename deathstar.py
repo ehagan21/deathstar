7ee3a5e7a47c86f7147d34f2182acbbf3d43f7a7
ehagan21 <ehagan21@gmail.com>
2015-12-16 02:11:18 -0500

    shifting to dstar

--- deathstar.py
+++ deathstar.py
@@ -1,13 +1,42 @@
-import cv2.cv as cv
+"""Deathstar takes a camera feed from a USB camera (using pygame) and spits it out onto
+a 480 x 320 monitor which is attached to a helmet, showing the cameras path through a 
+rotating felt Death Star"""
+
+#This code borrows heavily from pygame.cam example and also includes other example code
+
+# import the relevant libraries
 import time
+import pygame
+import pygame.camera
+from pygame.locals import *
+# this is where one sets how long the script
+# sleeps for, between frames.
+sleeptime__in_seconds = 0.05
+# initialise the display window
+screen = pygame.display.set_mode((480, 320),FULLSCREEN)
+pygame.init()
+pygame.camera.init()
+# set up a camera object
+cam = pygame.camera.Camera("/dev/video0",(480,320))
+# start the camera
+cam.start()
 
-cv.NamedWindow("camera", 0)
+while 1:
 
-capture = cv.CaptureFromCAM(0)
+    # sleep between every frame
+    time.sleep( sleeptime__in_seconds )
+    # fetch the camera image
+    image = cam.get_image()
+    # blank out the screen
+    screen.fill([0,0,0])
+    # copy the camera image to the screen
+    screen.blit( image, ( 0, 20 ) )
+    # update the screen to show the latest screen image
+    pygame.display.update()
 
-while True:
-    img = cv.QueryFrame(capture)
-    cv.ShowImage("camera", img)
-    if cv.WaitKey(10) == 27:
-        break
-cv.DestroyAllWindows()
+    events = pygame.event.get()
+    for e in events:
+        if e.type == QUIT or (e.type == KEYDOWN and e.key == K_ESCAPE):
+            # close the camera safely
+            cam.stop()
+            pygame.quit()
